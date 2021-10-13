4a363809f60fac611a50c9d5047b50166e9889e3
Husain <ochy22@gmail.com>
2021-10-13 17:02:41 +0800

    no message

---
+++ index.php
@@ -0,0 +1,196 @@
+<?php
+
+$artikel = array (
+  (object) array (
+    "gambar" => "https://images.unsplash.com/photo-1556595101-15dc5f6431e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "judul" => "Title 1",
+    "text" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem deserunt dignissimos, doloribus minus reprehenderit laborum molestiae fugit corrupti nesciunt repellendus accusantium quod magnam harum nobis! Asperiores obcaecati dignissimos recusandae."
+  ),
+  (object) array (
+    "gambar" => "https://images.unsplash.com/photo-1556595101-15dc5f6431e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "judul" => "Title 2",
+    "text" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem deserunt dignissimos, doloribus minus reprehenderit laborum molestiae fugit corrupti nesciunt repellendus accusantium quod magnam harum nobis! Asperiores obcaecati dignissimos recusandae."
+  ),
+  (object) array (
+    "gambar" => "https://images.unsplash.com/photo-1556595101-15dc5f6431e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "judul" => "Title 3",
+    "text" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem deserunt dignissimos, doloribus minus reprehenderit laborum molestiae fugit corrupti nesciunt repellendus accusantium quod magnam harum nobis! Asperiores obcaecati dignissimos recusandae."
+  ),
+  (object) array (
+    "gambar" => "https://images.unsplash.com/photo-1556595101-15dc5f6431e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "judul" => "Title 4",
+    "text" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem deserunt dignissimos, doloribus minus reprehenderit laborum molestiae fugit corrupti nesciunt repellendus accusantium quod magnam harum nobis! Asperiores obcaecati dignissimos recusandae."
+  ), 
+  (object) array (
+    "gambar" => "https://images.unsplash.com/photo-1556595101-15dc5f6431e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "judul" => "Title 4",
+    "text" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem deserunt dignissimos, doloribus minus reprehenderit laborum molestiae fugit corrupti nesciunt repellendus accusantium quod magnam harum nobis! Asperiores obcaecati dignissimos recusandae."
+  ), 
+  (object) array (
+    "gambar" => "https://images.unsplash.com/photo-1556595101-15dc5f6431e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    "judul" => "Title 4",
+    "text" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem deserunt dignissimos, doloribus minus reprehenderit laborum molestiae fugit corrupti nesciunt repellendus accusantium quod magnam harum nobis! Asperiores obcaecati dignissimos recusandae."
+  ), 
+);
+
+$carousel = array (
+  (object) array (
+    "active" => "active",
+    "gambar" => "https://images.unsplash.com/photo-1501183638710-841dd1904471?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+    "judul" => "Title 1",
+    "text" => "text 2"
+  ),
+  (object) array (
+    "active" => "",
+    "gambar" => "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvbWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "judul" => "Title 2",
+    "text" => "text 2"
+  ),
+  (object) array (
+    "active" => "",
+    "gambar" => "https://images.unsplash.com/photo-1519643225200-94e79e383724?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80",
+    "judul" => "Title 3",
+    "text" => "text 2"
+  )
+)
+
+?>
+
+<html>
+  <head>
+    <title>Bootstrap</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" integrity="sha512-xnP2tOaCJnzp2d2IqKFcxuOiVCbuessxM6wuiolT9eeEJCyy0Vhcwa4zQvdrZNVqlqaxXhHqsSV1Ww7T2jSCUQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExample    Indicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+
+        <?php foreach ($carousel as $data) { ?>
+          <div class="carousel-item <?= $data->active ?>">
+            <img src="<?= $data->gambar ?>" class="carousel-img d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5><?= $data->judul ?></h5>
+              <p><?= $data->text ?></p>
+            </div>
+          </div>
+        <?php } ?>
+        
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+    <div id="features" class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col col-4">
+            <div class="card text-center py-3">
+              <div class="card-body">
+                <div class="icon mb-3">
+                  <i class="bi bi-alarm"></i>
+                </div>
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">desc</p>
+              </div>
+            </div>
+          </div>
+          <div class="col col-4">
+            <div class="card text-center py-3">
+              <div class="card-body">
+                <div class="icon mb-3">
+                  <i class="bi bi-alarm"></i>
+                </div>
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">desc</p>
+              </div>
+            </div>
+          </div>
+          <div class="col col-4">
+            <div class="card text-center py-3">
+              <div class="card-body">
+                <div class="icon mb-3">
+                  <i class="bi bi-alarm"></i>
+                </div>
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">desc</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="blog" class="py-5">
+      <div class="container">
+        <div class="row">
+
+          <?php foreach ($artikel as $data) { ?>
+            <div class="col col-lg-6">
+              <div class="card mb-3">
+                <img src="<?= $data->gambar ?>" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title"><?= $data->judul ?></h5>
+                  <p class="card-text"><?= $data->text ?></p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          <?php } ?>
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
